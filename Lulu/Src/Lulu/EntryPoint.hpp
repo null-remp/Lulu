c3585fa271f1215b87c25915954c3f094ed5baf7
constantine <constantinenull2223@gmail.com>
2023-04-03 14:21:14 +0300

    Added premake

--- Lulu/Src/Lulu/EntryPoint.hpp
+++ Lulu/Src/Lulu/EntryPoint.hpp
@@ -7,13 +7,11 @@ extern Lulu::Application* Lulu::CreateApplication();
 int main(int argc, char** argv)
 {
 	Lulu::Log::Init();
-	LU_CORE_WARN("Initialized Log!");
-	int a = 12;
-	LU_APP_INFO("Hello App! Var={0}", a);
 
+	LU_CORE_ERROR("HI");
 
 	auto sandbox = Lulu::CreateApplication();
-	sandbox->Run();
+	sandbox->Run(); 
 	delete sandbox;
 
 	return 0;
---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Lulu"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Lulu"
+	location "Lulu"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/Src/**.hpp",
+		"%{prj.name}/Src/**.h",
+		"%{prj.name}/Src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"LU_PLATFORM_WINDOWS",
+			"LU_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "LU_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "LU_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "LU_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/Src/**.hpp",
+		"%{prj.name}/Src/**.h",
+		"%{prj.name}/Src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Lulu/vendor/spdlog/include",
+		"Lulu/Src"
+	}
+
+	links
+	{
+		"Lulu"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"LU_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "LU_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "LU_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "LU_DIST"
+		optimize "On"
